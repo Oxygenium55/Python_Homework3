ccf4481eaa080e89c8b5e12b68418214da5e5843
Oxygen <grandnorik@mail.ru>
2022-12-25 11:59:21 +0600

    Task2 done

--- Task2.py
+++ Task2.py
@@ -4,6 +4,17 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
+my_list1 = [2, 3, 4, 5, 6]
+my_list2 = []
+result = 1
 
+for i in range(len(my_list1)):
+    index = len(my_list1) - 1
+    if i < len(my_list1) / 2:
+        result = my_list1[i] * my_list1[index - i]
+        my_list2.append(result)
+    index-=1
+
+print(f'{my_list1} => {my_list2}')
 
 
